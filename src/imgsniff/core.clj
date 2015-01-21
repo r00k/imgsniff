cc7c7865186da90a52e72fffe0c79b5be75c7146
Ben Orenstein <ben.orenstein@gmail.com>
2015-01-21 12:30:01 -0700

    Getting close

--- src/imgsniff/core.clj
+++ src/imgsniff/core.clj
@@ -1,6 +1,7 @@
 (ns imgsniff.core
   (:require [clojure.data.json :as json]
-            [clj-http.client :as client]))
+            [clj-http.client :as client]
+            [clojure.string :as str]))
 
 (def imgur-client-id "5e439e50bdd53fc")
 (def reddit-liked-url "http://www.reddit.com/user/friendlyproject/liked.json")
@@ -14,19 +15,29 @@
 
 (def urls (get-imgur-urls reddit-liked-url))
 
-(pprint urls)
-
-(defn expand-album-url [imgur-url]
-  "If imgur-url is an album, return the urls of all its images"
-  (if (is-album? imgur-url)
-    (get-album-urls imgur-url)
-    imgur-url))
-
-; TODO: is this working?
-; Then, do get-album-urls
+; TODO: just check if '/a' appears in url
 (defn is-album? [imgur-url]
   "Returns true if the url points to an album"
-  (let [http-status
-        (client/get "https://api.imgur.com/3/album/m9ki8/images"
-                    {:headers {"Authorization" (str "Client-ID " imgur-client-id)}})]
-    (not (= http-status 404))))
+  (let [response
+        (client/get imgur-url
+                    {:headers {"Authorization" (str "Client-ID " imgur-client-id)}
+                     :throw-exceptions false})]
+    (not (= 404 (:status response)))))
+
+;; (defn expand-album-url [imgur-url]
+;;   "If imgur-url is an album, return the urls of all its images"
+;;   (if (is-album? imgur-url)
+;;     (get-image-urls-from-album imgur-url)
+;;     imgur-url))
+
+(defn get-image-urls-from-album [imgur-url]
+  "Given a url for an imgur album, return the urls of all its images."
+  (let [album-id (last (str/split imgur-url #"/"))
+        api-url (str "https://api.imgur.com/3/album/" album-id "/images")
+        response (client/get api-url
+                {:headers {"Authorization" (str "Client-ID " imgur-client-id)}})
+        parsed-response (json/read-str (:body response))
+        data (get parsed-response "data")]
+    (map #(get % "link") data)))
+
+(get-image-urls-from-album "http://imgur.com/a/m9ki8")
