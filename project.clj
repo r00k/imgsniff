4cb4fe7fe3d6cc4a1efbb061d8d47ba19fa4227d
Ben Orenstein <ben.orenstein@gmail.com>
2015-01-21 11:04:48 -0700

    Make progress

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [clj-http "1.0.1"]]
   :main ^:skip-aot imgsniff.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/imgsniff/core.clj
+++ src/imgsniff/core.clj
@@ -1,7 +1,32 @@
 (ns imgsniff.core
-  (:gen-class))
+  (:require [clojure.data.json :as json]
+            [clj-http.client :as client]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def imgur-client-id "5e439e50bdd53fc")
+(def reddit-liked-url "http://www.reddit.com/user/friendlyproject/liked.json")
+
+(defn get-imgur-urls
+  [reddit-liked-url]
+  (-> (slurp reddit-liked-url)
+      (json/read-str)
+      (get-in ["data" "children"])
+      (->> (map #(get-in % ["data" "url"])))))
+
+(def urls (get-imgur-urls reddit-liked-url))
+
+(pprint urls)
+
+(defn expand-album-url [imgur-url]
+  "If imgur-url is an album, return the urls of all its images"
+  (if (is-album? imgur-url)
+    (get-album-urls imgur-url)
+    imgur-url))
+
+; TODO: is this working?
+; Then, do get-album-urls
+(defn is-album? [imgur-url]
+  "Returns true if the url points to an album"
+  (let [http-status
+        (client/get "https://api.imgur.com/3/album/m9ki8/images"
+                    {:headers {"Authorization" (str "Client-ID " imgur-client-id)}})]
+    (not (= http-status 404))))
